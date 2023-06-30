0e22848d04477c784fbeb29d869b9cffd27d6848
Aditipatidar-26 <129889436+Aditipatidar-26@users.noreply.github.com>
2023-06-30 13:48:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const signUpBtn=document.querySelector('.signin-btn');
+const signInBtn=document.querySelector('.signup-btn');
+const formsWrapper=document.querySelector('.forms-wrapper');
+
+signUpBtn.addEventListener("click",(e) => {
+    e.preventDefault();
+    formsWrapper.classList.remove("change");
+});
+signInBtn.addEventListener("click",(e) => {
+    e.preventDefault();
+    formsWrapper.classList.add("change");
+});
